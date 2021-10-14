b90642b71d5fbf4947d288b4def60605861978a9
gizemguzelg <90610379+gizemguzelg@users.noreply.github.com>
2021-10-14 15:37:05 +0300

    Add files via upload

---
+++ LinQ_Examples/LinQ_Examples/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace LinQ_Examples
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExample1 = new System.Windows.Forms.Button();
+            this.btnExample2 = new System.Windows.Forms.Button();
+            this.btnExample3 = new System.Windows.Forms.Button();
+            this.btnExample4 = new System.Windows.Forms.Button();
+            this.btnExample5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(496, 199);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnExample1
+            // 
+            this.btnExample1.Location = new System.Drawing.Point(12, 205);
+            this.btnExample1.Name = "btnExample1";
+            this.btnExample1.Size = new System.Drawing.Size(75, 23);
+            this.btnExample1.TabIndex = 1;
+            this.btnExample1.Text = "Example1";
+            this.btnExample1.UseVisualStyleBackColor = true;
+            this.btnExample1.Click += new System.EventHandler(this.btnExample1_Click);
+            // 
+            // btnExample2
+            // 
+            this.btnExample2.Location = new System.Drawing.Point(93, 205);
+            this.btnExample2.Name = "btnExample2";
+            this.btnExample2.Size = new System.Drawing.Size(75, 23);
+            this.btnExample2.TabIndex = 1;
+            this.btnExample2.Text = "Example2";
+            this.btnExample2.UseVisualStyleBackColor = true;
+            this.btnExample2.Click += new System.EventHandler(this.btnExample2_Click);
+            // 
+            // btnExample3
+            // 
+            this.btnExample3.Location = new System.Drawing.Point(174, 205);
+            this.btnExample3.Name = "btnExample3";
+            this.btnExample3.Size = new System.Drawing.Size(75, 23);
+            this.btnExample3.TabIndex = 1;
+            this.btnExample3.Text = "Example3";
+            this.btnExample3.UseVisualStyleBackColor = true;
+            this.btnExample3.Click += new System.EventHandler(this.btnExample3_Click);
+            // 
+            // btnExample4
+            // 
+            this.btnExample4.Location = new System.Drawing.Point(255, 205);
+            this.btnExample4.Name = "btnExample4";
+            this.btnExample4.Size = new System.Drawing.Size(75, 23);
+            this.btnExample4.TabIndex = 1;
+            this.btnExample4.Text = "Example4";
+            this.btnExample4.UseVisualStyleBackColor = true;
+            this.btnExample4.Click += new System.EventHandler(this.btnExample4_Click);
+            // 
+            // btnExample5
+            // 
+            this.btnExample5.Location = new System.Drawing.Point(336, 205);
+            this.btnExample5.Name = "btnExample5";
+            this.btnExample5.Size = new System.Drawing.Size(75, 23);
+            this.btnExample5.TabIndex = 1;
+            this.btnExample5.Text = "Example5";
+            this.btnExample5.UseVisualStyleBackColor = true;
+            this.btnExample5.Click += new System.EventHandler(this.btnExample5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExample1);
+            this.Controls.Add(this.btnExample2);
+            this.Controls.Add(this.btnExample3);
+            this.Controls.Add(this.btnExample4);
+            this.Controls.Add(this.btnExample5);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExample1;
+        private System.Windows.Forms.Button btnExample2;
+        private System.Windows.Forms.Button btnExample3;
+        private System.Windows.Forms.Button btnExample4;
+        private System.Windows.Forms.Button btnExample5;
+    }
+}
+
---
+++ LinQ_Examples/LinQ_Examples/Form1.cs
@@ -0,0 +1,145 @@
+﻿using LinQ_Examples.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.SqlServer;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinQ_Examples
+{
+    public partial class Form1 : Form
+    {
+        NorthwindEntities db;
+        public Form1()
+        {
+            db = new NorthwindEntities();
+            InitializeComponent();
+        }
+
+        private void btnExample1_Click(object sender, EventArgs e)
+        {
+            //Ürünlerin Id'sini,ürün adını,fiyatını getirin.
+            #region LinQ Entity 
+            dataGridView1.DataSource = db.Products.Select(x => new
+            { x.ProductID,
+              x.ProductName,
+              x.UnitPrice
+            }).ToList();
+
+            #endregion
+
+            #region LinQ SQL
+            var Products = from x in db.Products
+                           select new
+                           {
+                               x.ProductID,
+                               x.ProductName,
+                               x.UnitPrice
+                           };
+            dataGridView1.DataSource = Products.ToList();
+               
+            #endregion
+
+        }
+
+        private void btnExample2_Click(object sender, EventArgs e)
+        {
+            //Ürünlerin Id'si 5 ile 15 arasında olanların A-Z'ye sıralayınız.
+            #region LinQ to Entity
+            dataGridView1.DataSource = db.Products.Where(x => x.ProductID >= 2 && x.ProductID<=15)
+                                                .OrderBy(x => x.ProductName)
+                                                .Select(x => new
+                                                {
+                                                    x.ProductID,
+                                                    x.ProductName,
+                                                    x.UnitsInStock
+                                                }).ToList();
+            #endregion
+
+            #region LinQ to SQL
+            var Products = from x in db.Products
+                           where x.ProductID >= 5 && x.ProductID <= 15
+                           orderby x.ProductID
+                           select new
+                           {
+                               x.ProductID,
+                               x.ProductName,
+                               x.UnitsInStock
+                           };
+            dataGridView1.DataSource = Products.ToList();
+
+
+            #endregion
+        }
+
+        private void btnExample3_Click(object sender, EventArgs e)
+        {//Soru:50 yaşından büyük olan çalışanları büyükten küçüğe listeleyiniz.
+            #region
+            dataGridView1.DataSource = db.Employees.Where(x => SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now) > 50)
+                                                 .OrderByDescending(x => SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now) >= 50)
+                                                 .Select(x => new
+                                                 {
+                                                     FullName = x.FirstName + "" + x.LastName,
+                                                     Age = SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now)
+                                                 }).OrderByDescending(x => x.Age)
+                                                   .ToList();
+            #endregion
+
+            #region LinQ to SQL
+            var result = from emp in db.Employees
+                         where SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) > 50
+                         orderby SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) descending
+                         select new
+                         {
+                             Adı = emp.FirstName,
+                             Soyadı = emp.LastName,
+                             Yas = SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now)
+                         };
+           dataGridView1.DataSource = result.ToList();
+            #endregion
+        }
+
+        private void btnExample4_Click(object sender, EventArgs e)
+        {
+            //Ünvanı Mr ve yaşı 50'den büyük olan çalışanları listeleyiniz.
+            #region LİnQ to Entity
+            dataGridView1.DataSource = db.Employees.Where(x => x.TitleOfCourtesy == "Mr" && SqlFunctions.DateDiff("Year", x.BirthDate, DateTime.Now) > 50)
+                                                   .ToList();
+            #endregion
+
+            #region LinQ to SQL
+            var result = from emp in db.Employees
+                         where emp.TitleOfCourtesy == "Mr" && SqlFunctions.DateDiff("Year", emp.BirthDate, DateTime.Now) > 50
+                         select new
+                         {
+                             FullName = emp.LastName + "" + emp.FirstName,
+                             emp.TitleOfCourtesy
+                         };
+
+
+            dataGridView1.DataSource = result.ToList();
+            #endregion
+
+        }
+
+        private void btnExample5_Click(object sender, EventArgs e)
+        {
+            //Kategorilere göre toplam stok miktarını getiriniz.
+            dataGridView1.DataSource = db.Categories
+                .Join(db.Products, c => c.CategoryID, p => p.CategoryID, (c, p) => new { c, p })
+                .GroupBy(x => x.c.CategoryID)
+                .Select(x => new
+                {
+                    CategoryId = x.Key,
+                    count = x.Sum(z => z.p.UnitsInStock)
+                })
+                .OrderByDescending(x => x.count)
+                .ToList();   
+        }
+    }
+}
