3f72dba7690505d1b592e3b8cab245eb2b7261dd
gizemguzelg <90610379+gizemguzelg@users.noreply.github.com>
2021-10-22 22:38:12 +0300

    Add files via upload

---
+++ Repository/Concrete/PhoneNumberRepository.cs
@@ -0,0 +1,115 @@
+﻿using Dapper;
+using DapperExample.Entities;
+using DapperExample.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DapperExample.Repository.Concrete
+{
+    public class PhoneNumberRepository : IBaseRepository<Phone>
+    {
+        SqlConnection db = new SqlConnection(@"Data Source = GIZEM; Initial Catalog = DapperExample; Integrated Security = True");
+
+        public bool Delete(Phone item)
+        {
+            try
+            {
+                sqlOpen();
+                db.Query<Phone>(@"DELETE FROM [dbo].[Phone] where Id=@id", item);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+            finally
+            {
+                sqlClose();
+            }
+        }
+
+        public List<Phone> GetAll()
+        {
+            try
+            {
+                sqlOpen();
+
+                List<Phone> list = db.Query<Phone>("SELECT * FROM  [dbo].[Phone]").ToList();
+                return list;
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message.ToString());
+            }
+            finally
+            {
+                sqlClose();
+            }
+        }
+
+        internal void Save(Phone getPhone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Phone item)
+        {
+            try
+            {
+                sqlOpen();
+                db.Query<Phone>(@"INSERT INTO  [dbo].[Phone]([Id],[FullName],[PhoneNumber]) VALUES (@Id,@FullName,@PhoneNumber)",item);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Insert hatası "+ex.Message.ToString());
+            }
+            finally
+            {
+                sqlClose();
+            }
+        }
+
+        public bool Update(Phone item)
+        {
+            try
+            {
+                sqlOpen();
+                db.Query<Phone>(@"UPDATE [dbo].[Phone] SET 
+                                         [Id]=@Id,
+                                         [FullName]=@FullName,
+                                         [PhoneNumber]=@PhoneNumber", item);
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message.ToString());
+            }
+            finally
+            {
+                sqlClose();
+            }
+        }
+        public void sqlOpen()
+        {
+            if (db.State == System.Data.ConnectionState.Closed)
+                db.Open();
+        }
+        public void sqlClose()
+        {
+            if (db.State == System.Data.ConnectionState.Open)
+                db.Close();
+        }
+
+    }
+}
---
+++ Repository/Interface/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.Repository.Interface
+{
+    public interface IBaseRepository<T>
+    {
+        List<T> GetAll();
+        bool Insert(T item);
+        bool Update(T item);
+        bool Delete(T item);
+
+
+
+    }
+}
